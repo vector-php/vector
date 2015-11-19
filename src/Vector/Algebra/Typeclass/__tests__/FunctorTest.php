38c0afcc9decd9ff90e7dabce8c5beeaf43c453c
Logan Henson <logan@loganhenson.com>
2015-11-19 17:00:46 -0600

    tests

---
+++ src/Vector/Algebra/Typeclass/__tests__/FunctorTest.php
@@ -0,0 +1,9 @@
+<?php
+
+class FunctorTest extends PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ src/Vector/Algebra/Typeclass/__tests__/MonadTest.php
@@ -0,0 +1,9 @@
+<?php
+
+class MonadTest extends PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+        $this->assertTrue(true);
+    }
+}

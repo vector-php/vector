4deeea8022ad413f7c42b546e7ae3be90e83c4a9
Joseph Walker <joseph@gravity4.com>
2016-01-28 12:19:17 -0600

    Added tests for lens, sans implementation.

--- tests/Control/LensTest.php
+++ tests/Control/LensTest.php
@@ -2,10 +2,43 @@
 
 namespace Vector\Test\Core;
 
+use Vector\Control\Lens;
+
 class LensTest extends \PHPUnit_Framework_TestCase
 {
-    public function testPlaceholder()
+    public function testLensesCanBeCreated()
     {
-        $this->assertEquals(2, 1 + 1);
+        list($indexLens, $propLens) = Lens::using('indexLens', 'propLens');
+
+        $testIndex = $indexLens(0);
+        $testProp  = $propLens('a');
+
+        $this->assertInstanceOf('\\Closure', $testIndex);
+        $this->assertInstanceOf('\\Closure', $testProp);
+    }
+
+    public function testViewThroughLens()
+    {
+
+    }
+
+    public function testSetThroughLens()
+    {
+
+    }
+
+    public function testSetDoesNotMutateData()
+    {
+
+    }
+
+    public function testMapOverLens()
+    {
+
+    }
+
+    public function testLensComposition()
+    {
+
     }
 }

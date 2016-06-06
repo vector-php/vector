d56b1a775b920bcc41edcea1ccc5949aa6195558
Logan Henson <logan@loganhenson.com>
2016-06-05 19:28:41 -0500

    added  to Lib/Lambda

--- src/Lib/Lambda.php
+++ src/Lib/Lambda.php
@@ -4,9 +4,17 @@ namespace Vector\Lib;
 
 use Vector\Core\Module;
 
+/**
+ * @method static callable pipe() pipe(...$fs)
+ * @method static callable compose() compose(...$fs)
+ * @method static callable k() k($k)
+ * @method static mixed id() id($a)
+ */
 abstract class Lambda extends Module
 {
-    protected static function pipe(...$fs)
+    protected static $dirtyHackToEnableIDEAutocompletion = true;
+
+    protected static function _pipe(...$fs)
     {
         return function(...$args) use ($fs) {
             $carry = null;
@@ -21,9 +29,9 @@ abstract class Lambda extends Module
         };
     }
 
-    protected static function compose(...$fs)
+    protected static function _compose(...$fs)
     {
-        return self::pipe(...array_reverse($fs));
+        return self::_pipe(...array_reverse($fs));
     }
 
     /**
@@ -45,7 +53,7 @@ abstract class Lambda extends Module
      * @param  mixed    $k Value to express in the combinator
      * @return \Closure    Expression which always returns $k
      */
-    protected static function k($k)
+    protected static function _k($k)
     {
         return function(...$null) use ($k)
         {
@@ -68,7 +76,7 @@ abstract class Lambda extends Module
      * @param  mixed $a Value to return
      * @return mixed    The given value, unchanged
      */
-    protected static function id($a)
+    protected static function _id($a)
     {
         return $a;
     }

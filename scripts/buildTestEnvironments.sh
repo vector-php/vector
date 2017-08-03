bbb73539816d194536165cb71f986f5cea7fa3f0
Logan Henson <logan@loganhenson.com>
2017-08-03 16:19:55 -0500

    update builds to only target 7.1 and nightly, allow failures for nightly

--- scripts/buildTestEnvironments.sh
+++ scripts/buildTestEnvironments.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-echo "Building testing environment for PHP7.0..."
-docker build -t vector/tests-php7.0 ./environments/php7.0/
-
 echo "Building testing environment for PHP7.1..."
 docker build -t vector/tests-php7.1 ./environments/php7.1/
+
+echo "Building testing environment for PHP-Nightly..."
+docker build -t vector/tests-php-nightly ./environments/php-nightly/
--- scripts/runTests.sh
+++ scripts/runTests.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-echo "Runing tests in PHP7.0..."
-docker run -v $PWD:/home/vector/ vector/tests-php7.0
-
 echo "Runing tests in PHP7.1..."
 docker run -v $PWD:/home/vector/ vector/tests-php7.1
+
+echo "Runing tests in PHP-Nightly..."
+docker run -v $PWD:/home/vector/ vector/tests-php-nightly

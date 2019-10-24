b0b8bd112e7c92df951e9711527cce789243eb66
Logan Henson <logan@loganhenson.com>
2019-10-24 12:34:29 -0500

    Update to official 7.4 image & Remove nightly tests.

--- scripts/buildTestEnvironments.sh
+++ scripts/buildTestEnvironments.sh
@@ -2,6 +2,3 @@
 
 echo "Building testing environment for PHP7.4..."
 docker build -t vector/tests-php7.4 ./environments/php7.4/
-
-echo "Building testing environment for PHP-Nightly..."
-docker build -t vector/tests-php-nightly ./environments/nightly/
--- scripts/runTests.sh
+++ scripts/runTests.sh
@@ -2,6 +2,3 @@
 
 echo "Running tests in PHP7.4..."
 docker run -v $PWD:/home/vector/ vector/tests-php7.4
-
-echo "Running tests in PHP-Nightly..."
-docker run -v $PWD:/home/vector/ vector/tests-php-nightly
